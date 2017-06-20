9ad1a7e67e653e069c5f3eadd0933416e876fbe8
Mike Mull <mike.mull@gmail.com>
2017-06-19 21:31:16 -0700

    Added exponential Almon

--- midas/weights.py
+++ midas/weights.py
@@ -12,7 +12,30 @@ def beta_weights_es(n, theta1, theta2):
     return beta_vals / sum(beta_vals)
 
 
+def exp_almon_weights(lag, k1, k2):
+    """
+    Exponential Almon weights
+
+    Returns:
+        array: Array of weights
+
+    """
+    ilag = np.arange(1, lag + 1)
+    z = np.exp((k1 * ilag) + (k2 * ilag) ** 2)
+    return z / sum(z)
+
+
 def x_weighted(x, theta1, theta2):
+    """
+    Weight the matrix of regressors according to the specified weighting method
+    Args:
+        x:
+        theta1:
+        theta2:
+
+    Returns:
+
+    """
     w = beta_weights_es(x.shape[1], theta1, theta2)
 
     return np.dot(x, w), np.tile(w.T, (x.shape[1], 1))
--- tests/test_weights.py
+++ tests/test_weights.py
@@ -1,12 +1,18 @@
 import numpy as np
 
-from midas.weights import beta_weights_es, x_weighted
+from midas.weights import beta_weights_es, x_weighted, exp_almon_weights
 
 
 def test_beta_es():
     w = beta_weights_es(3, 1, 5)
 
     assert np.allclose(w, [0.941176, 0.0588238, 9.4118e-25])
+
+
+def test_almon():
+    w = exp_almon_weights(3, -1., 0.)
+
+    assert np.allclose(w, [0.665241, 0.244728, 0.090031])
 
 
 def test_x_weighted():
